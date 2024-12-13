4215c9ea093e64c652fb517585a5d26a80aa1578
Gabriella Caroline PN <lalagabriella03@gmail.com>
2024-12-13 10:40:17 +0700

    edit tampilan tabel data peminjam supaya sama dengan tabel inventoris

--- resources/views/admin/datapeminjam.blade.php
+++ resources/views/admin/datapeminjam.blade.php
@@ -26,121 +26,59 @@
 @endpush
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 flex align-items-stretch">
-            <div class="card w-100">
-                <div class="card-body">
-                    <h5>Data Peminjam</h5><br>
-                    @if (@session('success'))
-                        <div class="alert alert-success">{{ session('success') }}</div><br>
-                    @endif
-                    <div class="table-responsive">
-                        <table class="table text-nowrap mb-0 align-middle" id="peminjamanTable">
-                            <thead class="text-dark fs-4">
-                                <tr style="background-color: #48BEC8">
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">No</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Nama Peminjam</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Jenis Peminjam</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">NIP/NIM</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Kontak</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Nama Alat</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Tanggal Peminjaman</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Tanggal Pengembalian</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Status</h6>
-                                    </th>
-                                    <th style="width: 100px" class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0 text-white">Action</h6>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody> 
-                                @foreach ($peminjaman as $key => $row)
-                                <tr>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $key + 1 }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->nama_peminjam }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->jenis_peminjam }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->nip_nim }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->kontak }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->nama_alat ?? 'N/A' }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->tanggal_peminjaman }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->tanggal_pengembalian }} 
-                                        </h6>
-                                    </td>
-                                    <td class="border-bottom-0 text-center">
-                                        <h6 class="fw-semibold mb-0">
-                                            {{ $row->status }} 
-                                        </h6>
-                                    </td>   
-                                    <td class="border-bottom-0 align-items-center text-center">
-                                        <div class="row" style="width: 100px; margin: 0 auto">
-                                            <div class="col-6 d-flex justify-content-center">
-                                                <a type="button" data-bs-toggle="modal" data-bs-target="#edit{{ $row->id }}" class="text-black text-center d-flex align-items-center justify-content-center" style="width: 40px; height: 40px;">
-                                                    <i class="fa-solid fa-eye"></i>
-                                                </a>
-                                                {{-- @include('admin.modal.editPeminjaman', array('row' => $row)) --}}
-                                            </div>
-                                            <div class="col-6 d-flex justify-content-center">
-                                                <a type="button" data-bs-toggle="modal" data-bs-target="#delete{{ $row->id }}" class="text-black text-center d-flex align-items-center justify-content-center" style="width: 40px; height: 40px;">
-                                                    <i class="fa-solid fa-pen-to-square"></i>
-                                                </a>
-                                                {{-- @include('admin.modal.deletePeminjaman', array('row' => $row)) --}}
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="container my-5">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h2 class="fw-bold">Data Peminjam</h2>
     </div>
+
+    @if (@session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <div class="table-responsive">
+        <table class="table table-bordered table-striped">
+            <thead class="table-header">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Peminjam</th>
+                    <th>Jenis Peminjam</th>
+                    <th>NIP/NIM</th>
+                    <th>Kontak</th>
+                    <th>Nama Alat</th>
+                    <th>Tanggal Peminjaman</th>
+                    <th>Tanggal Pengembalian</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($peminjaman as $key => $row)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $row->nama_peminjam }}</td>
+                    <td>{{ $row->jenis_peminjam }}</td>
+                    <td>{{ $row->nip_nim }}</td>
+                    <td>{{ $row->kontak }}</td>
+                    <td>{{ $row->nama_alat ?? 'N/A' }}</td>
+                    <td>{{ $row->tanggal_peminjaman }}</td>
+                    <td>{{ $row->tanggal_pengembalian }}</td>
+                    <td>
+                        <span class="badge {{ $row->status === 'Dipinjam' ? 'bg-not-available' : 'bg-available' }}">
+                            {{ $row->status }}
+                        </span>
+                    </td>
+                    <td>
+                        <button class="btn btn-edit" data-bs-toggle="modal" data-bs-target="#edit{{ $row->id }}" title="Edit">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button class="btn btn-delete" data-bs-toggle="modal" data-bs-target="#delete{{ $row->id }}" title="Delete">
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
